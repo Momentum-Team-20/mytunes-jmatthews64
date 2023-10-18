5366d03067df88cb7ef6a8ac2124b645e147a796
jmatthews64 <jon.matthews64@gmail.com>
2023-10-18 17:18:38 -0400

    created variables for query selector and assigned them

---
+++ main.js
@@ -0,0 +1,7 @@
+//Grabbing all the items needed from HTML for input and output
+let searchBar = document.querySelector("#searchBar");
+let searchButton = document.querySelector("#searchButton");
+const musicContainer = document.querySelector("#mainFrame");
+
+
+
