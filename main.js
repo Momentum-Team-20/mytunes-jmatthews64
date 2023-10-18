c21cb3efd569936b263945551812fa44328378d1
jmatthews64 <jon.matthews64@gmail.com>
2023-10-18 18:02:27 -0400

    Greated input but attmepting to get it to pull in data

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 //Grabbing all the items needed from HTML for input and output
-let searchBar = document.querySelector("#searchBar");
+let searchInput = document.querySelector("#searchInput");
 let searchButton = document.querySelector("#searchButton");
 const musicContainer = document.querySelector("#mainFrame");
 
-
+searchButton.addEventListener('click', () => {
+    if (searchInput == ""){
+        alert("Please enter search criteria!")
+    }
+    
+})
 
