174ddf827379b6c1ec2c9d3e1eeb5bea44c9c1de
jmatthews64 <jon.matthews64@gmail.com>
2023-10-19 10:32:55 -0400

    Updated main.js to try to add API request

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ let searchButton = document.querySelector("#searchButton");
 const musicContainer = document.querySelector("#mainFrame");
 
 searchButton.addEventListener('click', () => {
-    if (searchInput == ""){
-        alert("Please enter search criteria!")
-    }
+    // if (searchInput == ""){
+    //     alert("Please enter search criteria!")
+    // } else {
+        fetch("https://itunes.apple.com/search?alan+jackson")
+        .then((Response) => {
+            return Response.json();
+        }).then((data) => {
+            console.log(data);
+            let album = data.results[0];
+            let albumDiv = document.createElement('div');
+            albumDiv.innerText = `Song: ${album.trackName}`
+            musicContainer.appendChild(albumDiv);
+        })
+    //}
     
 })
 
