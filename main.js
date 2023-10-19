15923696a591347fb3459e663cc3d3c039b2b4cb
jmatthews64 <jon.matthews64@gmail.com>
2023-10-19 11:09:53 -0400

    Created buildDisplay function to parse data from API

--- main.js
+++ main.js
@@ -11,26 +11,27 @@ searchButton.addEventListener('click', () => {
         .then((Response) => {
             return Response.json();
         }).then((data) => {
-            console.log(data);
-            let results = data.results;
-            for (let album of results){
-                //Create a box to put each result in
-                let box = document.createElement('div');
-                box.classList.add("albumBox");
-                musicContainer.appendChild(box);
-
-                //add album artwork
-                let albumArt = document.createElement('img');
-                albumArt.src = album.artworkUrl100;
-                albumArt.alt = "Album Cover";
-                box.appendChild(albumArt);
-
-                let albumDiv = document.createElement('div');
-                albumDiv.innerText = `Song: ${album.trackName}`
-                box.appendChild(albumDiv);
-            }
+            buildDisplay(data.results);
         })
     }
     
 })
 
+function buildDisplay(results){
+    for (let album of results){
+        //Create a box to put each result in
+        let box = document.createElement('div');
+        box.classList.add("albumBox");
+        musicContainer.appendChild(box);
+
+        //add album artwork
+        let albumArt = document.createElement('img');
+        albumArt.src = album.artworkUrl100;
+        albumArt.alt = "Album Cover";
+        box.appendChild(albumArt);
+
+        let albumDiv = document.createElement('div');
+        albumDiv.innerText = `Song: ${album.trackName}`
+        box.appendChild(albumDiv);
+    }
+}
